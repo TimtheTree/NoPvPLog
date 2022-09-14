ec0a59a3ade2a0b76c8627d20e7beb5134c71552
Shreb <Shrebtv@gmail.com>
2022-09-14 23:04:04 +0200

    A little bit of documentation so we dont get confused later on

--- src/main/java/de/ttt/nopvplog/models/CombatTimer.java
+++ src/main/java/de/ttt/nopvplog/models/CombatTimer.java
@@ -33,16 +33,30 @@ public class CombatTimer {
         return (this.lastDamage - System.currentTimeMillis()) / 1000;
     }
 
+    /**
+     *
+     * @param timerDuration The time a player has to wait before being out of combat
+     * @param minimumDeactivationDistance The distance the player has to be away from their enemy in order to be out of combat
+     * @return true if the combat timer is over AND the enemy players distance is greater than the minimum deactivation distance
+     */
     public boolean isOutOfCombat(long timerDuration, long minimumDeactivationDistance) {
         return timePassed() > timerDuration && playerEnemyDistance() > minimumDeactivationDistance;
     }
 
+    /**
+     * Calculates the distance of the player this timer is assigned to from the person who last hit them
+     * @return the distance
+     */
     public long playerEnemyDistance() {
         Player player = Bukkit.getPlayer(playerReference);
         Player enemy = Bukkit.getPlayer(enemyReference);
         return (long) player.getLocation().distance(enemy.getLocation());
     }
 
+    /**
+     * updates this objects last damage time and Enemy who cause the damage
+     * @param event the event to read from
+     */
     public void update(EntityDamageByEntityEvent event) {
         setLastDamage(System.currentTimeMillis());
         setEnemyReference(event.getDamager().getUniqueId());
