36e2d03c2b62ba7d0b4b00b46c329bd85b91a72f
Shreb <Shrebtv@gmail.com>
2022-09-14 23:05:44 +0200

    A little bit of documentation so we dont get confused later on

--- src/main/java/de/ttt/nopvplog/models/DeathCrate.java
+++ src/main/java/de/ttt/nopvplog/models/DeathCrate.java
@@ -19,14 +19,26 @@ public class DeathCrate {
         this.owner = owner;
     }
 
+    /**
+     * Fills both crates of a player who just combat logged
+     * @param player the player to fill the crates from
+     */
     public void fillCrates(Player player) {
 
     }
 
+    /**
+     * Fills the main (bottom) crate from the players inventory
+     * @param player
+     */
     private void fillMain(Player player) {
 
     }
 
+    /**
+     * Fills the equipment (top) crate from the players hotbar and Armor
+     * @param player
+     */
     private void fillEquip(Player player) {
 
     }
