d5894e7d4a41c325be5ebdd8115a699e9871143d
TimtheTree <tim@verbout.de>
2022-09-14 22:27:40 +0200

    Death Crate Model

--- src/main/java/de/ttt/nopvplog/models/DeathCrate.java
+++ src/main/java/de/ttt/nopvplog/models/DeathCrate.java
@@ -1,4 +1,33 @@
 package de.ttt.nopvplog.models;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import java.util.UUID;
+
 public class DeathCrate {
+
+    private final Inventory mainInv;
+
+    private final Inventory equipInv;
+
+    private final UUID owner;
+
+    public DeathCrate(Inventory mainInv, Inventory equipInv, UUID owner) {
+        this.mainInv = mainInv;
+        this.equipInv = equipInv;
+        this.owner = owner;
+    }
+
+    public void fillCrates(Player player) {
+
+    }
+
+    private void fillMain(Player player) {
+
+    }
+
+    private void fillEquip(Player player) {
+
+    }
 }
