fe552543489dd324276b5a667be7e833f0757da4
TimtheTree <tim@verbout.de>
2022-09-15 19:11:44 +0200

    Changes by sebas

---
+++ src/main/java/de/ttt/nopvplog/exceptions/DeathCrateCreationException.java
@@ -0,0 +1,5 @@
+package de.ttt.nopvplog.exceptions;
+
+public class DeathCrateCreationException extends RuntimeException{
+    public DeathCrateCreationException(String s){super(s);}
+}
--- src/main/java/de/ttt/nopvplog/models/DeathCrate.java
+++ src/main/java/de/ttt/nopvplog/models/DeathCrate.java
@@ -1,5 +1,14 @@
 package de.ttt.nopvplog.models;
 
+import de.ttt.nopvplog.NoPvPLogTemplate;
+import de.ttt.nopvplog.exceptions.DeathCrateCreationException;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Container;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -8,16 +17,54 @@ import java.util.UUID;
 
 public class DeathCrate {
 
-    private final Inventory mainInv;
+    private Inventory mainInv;
 
-    private final Inventory equipInv;
+    private Inventory equipInv;
 
     private final UUID owner;
 
-    public DeathCrate(Inventory mainInv, Inventory equipInv, UUID owner) {
+    private final NoPvPLogTemplate template;
+
+    private final Material containerType;
+
+    public DeathCrate(Inventory mainInv, Inventory equipInv, UUID owner, NoPvPLogTemplate template) {
+
         this.mainInv = mainInv;
         this.equipInv = equipInv;
         this.owner = owner;
+        this.template = template;
+
+        String containerName = template.getConfig().getString("DeathCrateType");
+
+        try {
+
+            this.containerType = Material.valueOf(containerName.toUpperCase());
+
+        } catch (IllegalArgumentException e) {
+            throw new DeathCrateCreationException("Could not get a proper container type from config. Make sure it exists and is a container with 3x9 inventory space!");
+        }
+
+    }
+
+    public void createCrates(Location location){
+
+        Block blockBottom = location.getWorld().getBlockAt(location);
+        Block blockTop = blockBottom.getRelative(BlockFace.UP);
+
+        blockBottom.setType(containerType);
+        blockTop.setType(containerType);
+
+        if(!(blockBottom instanceof Container containerBottom)){
+            throw new DeathCrateCreationException("Could not get a proper container type from config. Make sure it exists and is a container with 3x9 inventory space!");
+        }
+
+        if(!(blockTop instanceof Container containerTop)){
+            throw new DeathCrateCreationException("Could not get a proper container type from config. Make sure it exists and is a container with 3x9 inventory space!");
+        }
+
+        this.mainInv = containerBottom.getInventory();
+        this.equipInv = containerTop.getInventory();
+
     }
 
     /**
