9dbb811fd4026c8973b6a5297fb597e234752d8a
Shreb <Shrebtv@gmail.com>
2022-09-17 00:40:25 +0200

    started implementation

--- src/main/java/de/ttt/nopvplog/controllers/ActionBarController.java
+++ src/main/java/de/ttt/nopvplog/controllers/ActionBarController.java
@@ -1,11 +1,13 @@
 package de.ttt.nopvplog.controllers;
 
 import de.ttt.nopvplog.NoPvPLogTemplate;
+import de.ttt.nopvplog.models.CombatTimerPvp;
+import de.ttt.nopvplog.models.DamageTimer;
 import de.ttt.nopvplog.models.Timer;
 import de.ttt.nopvplog.models.actionbar.ActionBarMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.event.entity.EntityDamageEvent;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -14,28 +16,39 @@ public class ActionBarController {
 
     private final HashMap<UUID, ActionBarMessage> actionBarMap = new HashMap<>();
     private final int updateDelay;
-    private final CombatTimerController combatTimerController;
-    private final DamageTimerController damageTimerController;
+    private final NoPvPLogTemplate template;
 
-    public ActionBarController(int updateDelay, CombatTimerController combatTimerController, DamageTimerController damageTimerController) {
+    public ActionBarController(int updateDelay, NoPvPLogTemplate template) {
         this.updateDelay = updateDelay;
-        this.combatTimerController = combatTimerController;
-        this.damageTimerController = damageTimerController;
+        this.template = template;
     }
 
-    private void updateBar(UUID playerID){
-        //TODO, update the bar of the player
+    private void updateBar(UUID playerID) {
+
+        DamageTimer damageTimer = (DamageTimer) this.template.getDTController().getTimer(playerID);
+        CombatTimerPvp combatTimer = (CombatTimerPvp) this.template.getCTController().getTimer(playerID);
+
+        Timer<EntityDamageEvent> longerTimer;
+
+        if(damageTimer.getLastDamage() <= combatTimer.getLastDamage()) {
+            //TODO
+        }
+
+
+
     }
 
-    private void updateAllBars(){
+    private void updateAllBars() {
 
     }
 
-    private List<Timer<EntityDamageEvent>> getRelevantTimers(){
+    private List<Timer<EntityDamageEvent>> getRelevantTimers() {
         return null;
     }
 
     public void runUpdateTime(long timerTicks) {
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(template, this::updateAllBars, 10, this.updateDelay);
 
     }
 
