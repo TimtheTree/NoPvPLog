1f0216c1e236cfbea5a2bbf732f9738919edddec
Shreb <Shrebtv@gmail.com>
2022-09-18 20:10:16 +0200

    Added methods to the template

--- src/main/java/de/ttt/nopvplog/models/Timer.java
+++ src/main/java/de/ttt/nopvplog/models/Timer.java
@@ -9,11 +9,15 @@ public abstract class Timer<T extends EntityDamageEvent> {
 
     protected final UUID playerReference;
     protected long lastDamage;
-    private TimerController<? extends EntityDamageEvent> timerController;
+    private final TimerController<? extends EntityDamageEvent> timerController;
+    private final long timerDuration;
+    private final long minimumDeactivationDistance;
 
     protected Timer(UUID playerReference, TimerController<? extends EntityDamageEvent> timerController) {
         this.playerReference = playerReference;
         this.timerController = timerController;
+        this.timerDuration = this.timerController.getTimerDuration();
+        this.minimumDeactivationDistance = this.timerController.getMinimumDeactivationDistance();
     }
 
     public void setLastDamage(long lastDamage) {
@@ -22,6 +26,22 @@ public abstract class Timer<T extends EntityDamageEvent> {
 
     public long getLastDamage() {
         return this.lastDamage;
+    }
+
+    public UUID getPlayerReference() {
+        return playerReference;
+    }
+
+    public TimerController<? extends EntityDamageEvent> getTimerController() {
+        return timerController;
+    }
+
+    public long getTimerDuration() {
+        return timerDuration;
+    }
+
+    public long getMinimumDeactivationDistance() {
+        return minimumDeactivationDistance;
     }
 
     /**
