a124f6e19f571163397f8d3601d174367d6a30cd
Shreb <Shrebtv@gmail.com>
2022-09-18 22:08:35 +0200

    Reformatted code

--- src/main/java/de/ttt/nopvplog/controllers/ActionBarController.java
+++ src/main/java/de/ttt/nopvplog/controllers/ActionBarController.java
@@ -7,9 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 public class ActionBarController {
@@ -48,7 +46,6 @@ public class ActionBarController {
             }
 
             message.display(timer.timeLeftOnTimer());
-
         }
     }
 
