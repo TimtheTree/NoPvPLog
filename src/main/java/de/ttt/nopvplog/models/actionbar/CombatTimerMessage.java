7dba9f9879ce5ee0ff1a7bc27126a29b73ab8956
Shreb <Shrebtv@gmail.com>
2022-09-18 19:08:35 +0200

    Created a class to represent the Action bar message a player will see once they take damage

--- src/main/java/de/ttt/nopvplog/models/actionbar/CombatTimerMessage.java
+++ src/main/java/de/ttt/nopvplog/models/actionbar/CombatTimerMessage.java
@@ -1,12 +1,37 @@
 package de.ttt.nopvplog.models.actionbar;
 
-public class CombatTimerMessage extends AbstractActionBarMessage{
-    protected CombatTimerMessage(String message, int currentTimer) {
-        super(message, currentTimer);
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public class CombatTimerMessage {
+    private static final String TIMER_PLACEHOLDER = "%time%";
+
+    private final String baseMessage;
+    private final UUID ownerId;
+
+    public CombatTimerMessage(String baseMessage, UUID ownerId) {
+        this.baseMessage = baseMessage;
+        this.ownerId = ownerId;
     }
 
-    @Override
-    public void update() {
-        super.update();
+    /**
+     * Sends the owner of this CombatTimerMessage
+     * @param time the time to be displayed to the player
+     */
+    public void display(long time){
+
+        Player player = Bukkit.getPlayer(this.ownerId);
+
+        if(player == null) return;
+
+        player.sendActionBar(Component.text(renderMessage(time)));
+
+    }
+
+    private String renderMessage(long time){
+        return this.baseMessage.replaceAll(TIMER_PLACEHOLDER, String.valueOf(time));
     }
 }
