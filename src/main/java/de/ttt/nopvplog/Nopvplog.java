7da64be3dad74350d2e81fc50b014c18d87f0cd9
Shreb <Shrebtv@gmail.com>
2022-09-18 20:06:10 +0200

    Made a new method for getting the timer which still has a longer time to run out. Added ActionBarController logic to the onEnable() method

--- src/main/java/de/ttt/nopvplog/Nopvplog.java
+++ src/main/java/de/ttt/nopvplog/Nopvplog.java
@@ -1,20 +1,27 @@
 package de.ttt.nopvplog;
 
+import de.ttt.nopvplog.controllers.ActionBarController;
 import de.ttt.nopvplog.controllers.CombatTimerController;
 import de.ttt.nopvplog.controllers.DamageTimerController;
 import de.ttt.nopvplog.controllers.DeathCrateController;
 import de.ttt.nopvplog.listeners.CombatDetector;
 import de.ttt.nopvplog.listeners.PvPLogDetector;
+import de.ttt.nopvplog.models.CombatTimerPvp;
+import de.ttt.nopvplog.models.DamageTimer;
+import de.ttt.nopvplog.models.Timer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 import java.io.File;
+import java.util.UUID;
 
 public final class Nopvplog extends NoPvPLogTemplate {
 
     private CombatTimerController combatTimerController;
     private DamageTimerController damageTimerController;
     private DeathCrateController deathCrateController;
+    private ActionBarController actionBarController;
 
     @Override
     public void onEnable() {
@@ -25,10 +32,13 @@ public final class Nopvplog extends NoPvPLogTemplate {
         this.combatTimerController = new CombatTimerController(this);
         this.damageTimerController = new DamageTimerController(this);
         this.deathCrateController = new DeathCrateController(this);
+        this.actionBarController = new ActionBarController(20, this);
+
         //register Listener for Combat detection
         Bukkit.getPluginManager().registerEvents(new CombatDetector(this), this);
         Bukkit.getPluginManager().registerEvents(new PvPLogDetector(this.deathCrateController, this.combatTimerController, this.damageTimerController), this);
 
+        this.actionBarController.runUpdateTimer();
 
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Enabled NoPvPLog");
     }
@@ -61,5 +71,21 @@ public final class Nopvplog extends NoPvPLogTemplate {
     @Override
     public DeathCrateController getDeathCrateController() {
         return this.deathCrateController;
+    }
+
+    @Override
+    public Timer<? extends EntityDamageEvent> getLongerTimer(UUID playerId) {
+
+        DamageTimer damageTimer = (DamageTimer) this.getDTController().getTimer(playerId);
+        CombatTimerPvp combatTimer = (CombatTimerPvp) this.getCTController().getTimer(playerId);
+
+        Timer<? extends EntityDamageEvent> longerTimer;
+
+        if (damageTimer.timeLeftOnTimer() <= combatTimer.timeLeftOnTimer()) {
+            longerTimer = combatTimer;
+        } else {
+            longerTimer = damageTimer;
+        }
+        return longerTimer;
     }
 }
