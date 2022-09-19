0c913ad18a9c301fc9eca96a515fbfbf2941ecec
Shreb <Shrebtv@gmail.com>
2022-09-19 21:31:27 +0200

    Added comments, general maintenance

--- src/main/java/de/ttt/nopvplog/models/actionbar/CombatTimerMessage.java
+++ src/main/java/de/ttt/nopvplog/models/actionbar/CombatTimerMessage.java
@@ -6,40 +6,31 @@ import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
-public class CombatTimerMessage{
+public record CombatTimerMessage(String baseMessage, UUID ownerId) {
     private static final String TIMER_PLACEHOLDER = "%time%";
-
-    private final String baseMessage;
-    private final UUID ownerId;
-
-    public CombatTimerMessage(String baseMessage, UUID ownerId) {
-        this.baseMessage = baseMessage;
-        this.ownerId = ownerId;
-    }
-
-    public String getBaseMessage() {
-        return baseMessage;
-    }
-
-    public UUID getOwnerId() {
-        return ownerId;
-    }
 
     /**
      * Sends the owner of this CombatTimerMessage
+     *
      * @param time the time to be displayed to the player
      */
-    public void display(long time){
+    public void display(long time) {
 
         Player player = Bukkit.getPlayer(this.ownerId);
 
-        if(player == null) return;
+        if (player == null) return;
 
         player.sendActionBar(Component.text(renderMessage(time)));
 
     }
 
-    private String renderMessage(long time){
+    /**
+     * Replaces the time placeholder for the actual time
+     *
+     * @param time the time to display
+     * @return the fully rendered message
+     */
+    private String renderMessage(long time) {
         return this.baseMessage.replaceAll(TIMER_PLACEHOLDER, String.valueOf(time));
     }
 }
